8724ac2a87dffb659df487376a21da8aa27c5330
Vincent JARDIN <vjardin@free.fr>
2019-02-15 00:44:56 +0100

    CI: appveyor
    First drop of appveyor CI. Currently, it is mostly a cut'n paste
    from google's findings.

---
+++ appveyor/test.bat
@@ -0,0 +1,36 @@
+@echo off
+
+cd /d C:\projects\php-src
+
+set PHP_MODULES=php_bonjour
+
+rmdir /Q /S tests
+
+setlocal enableextensions enabledelayedexpansion
+
+set EXTENSIONTOREMOVE=Zend sapi sapi\phpdbg sapi\fpm sapi\cli sapi\cgi ext\standard ext\date ext\spl ext\reflection ext\pcre ext\spl\examples
+
+for %%e in (%EXTENSIONTOREMOVE%) do (
+  set EXTNAME=%%e
+  echo Remove tests in !EXTNAME!\tests
+  rmdir /Q /S !EXTNAME!\tests
+)
+
+::for %%a in (%ARCHITECTURES%) do (
+::set ARCH=%%a
+  set ARCH=%PLATFORM%
+  set SDK_RUNNER=%PHP_BUILD_CACHE_SDK_DIR%\phpsdk-%PHP_BUILD_CRT%-!ARCH!.bat
+  if not exist "!SDK_RUNNER!" (
+    echo "!SDK_RUNNER!" doesn't exist
+    exit /b 3
+  )
+
+  cmd /c !SDK_RUNNER! -t %APPVEYOR_BUILD_FOLDER%\appveyor\test_task.bat
+
+  if %errorlevel% neq 0 exit /b 3
+
+::)
+
+endlocal
+
+exit /b 0
